f4ccb5a932cfd67feb6f2533d4fc66120435726a
mehdicharife <mehdicharife@yahoo.com>
2023-09-02 00:11:09 +0100

    introducing-type-eloquence

--- src/element_type_comparator.c
+++ src/element_type_comparator.c
@@ -1,9 +1,21 @@
 #include "../include/element_type_comparator.h"
+#include <string.h>
+#include <stdio.h>
+
+int get_index(char** array, size_t array_size, char* value) {
+    for(size_t i = 0; i < array_size; i++) {
+        if(!strcmp(array[i], value)) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 int element_type_cmpr_eloquence(element_type* ptype1, element_type* ptype2) {
-    if(ptype1->name == ptype2->name) {
-        return 0;
-    }
+    char* ordered_names[] = {"string", "double", "float", "int"};
+    
+    int k1 = get_index(ordered_names, 4, ptype1->name);
+    int k2 = get_index(ordered_names, 4, ptype2->name);
 
-    return 2;
+    return k2 - k1;
 }
--- tests/element_type_comparator_test.c
+++ tests/element_type_comparator_test.c
@@ -2,20 +2,24 @@
 #include <criterion/new/assert.h>
 #include "../include/element_type_comparator.h"
 #include "../include/double_element_type.h"
+#include "../include/int_element_type.h"
 
 
-element_type* (*type_factory)();
 
-void setup(void) {
-    type_factory = new_double_element_type;
-}
-
-TestSuite(element_type_comparator_test_suite, .init=setup);
+TestSuite(element_type_comparator_test_suite);
 
 Test(element_type_comparator_test_suite, equality) {
-    element_type* ptype1 = type_factory();
-    element_type* ptype2 = type_factory();
+    element_type* ptype1 = new_double_element_type();
+    element_type* ptype2 = new_double_element_type();
 
     int comparison_result = element_type_cmpr_eloquence(ptype1, ptype2);
     cr_expect(eq(int, comparison_result, 0)); 
 }
+
+Test(element_type_comparator_test_sute, double_is_more_eloquent_than_int) {
+    element_type* pdbl_type = new_double_element_type();
+    element_type* pint_type = new_int_element_type();
+
+    int comparison_result = element_type_cmpr_eloquence(pdbl_type, pint_type);
+    cr_expect(gt(int, comparison_result, 0));
+ }
